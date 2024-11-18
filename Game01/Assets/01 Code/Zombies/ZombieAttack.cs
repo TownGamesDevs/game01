a6ae2d9a2f3e042f530018a2790a8edaa9de8d82
TownGames <lg470@hotmail.com>
2024-11-18 20:11:46 +0200

    18.11.24 - B

--- Game01/Assets/01 Code/Zombies/ZombieAttack.cs
+++ Game01/Assets/01 Code/Zombies/ZombieAttack.cs
@@ -39,9 +39,8 @@ public class ZombieAttack : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {
             _canAttack = true;
-        }
+        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
